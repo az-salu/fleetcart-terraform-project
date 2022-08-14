cf0fb04664e21a418e4087a0b4bafe850c03d6b2
Azeez Salu <aosnotes77@gmail.com>
2022-08-14 04:36:10 -0400

    update

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 # store the terraform state file in s3
 terraform {
   backend "s3" {
-    bucket    = "aosnotes77-dev-terraform-remote-state"
+    bucket    = "aosnotes77-terraform-remote-state"
     key       = "terraform.tfstate.dev"
     region    = "us-east-1"
     profile   = "terraform-user"
