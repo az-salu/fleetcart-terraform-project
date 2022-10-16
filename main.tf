b2f8c73b810e8f3bdd1591d46cba3b68985af55a
Azeez Salu <aosnotes77@gmail.com>
2022-10-16 13:14:13 -0400

    updated state file

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 # configured aws provider with proper credentials
 provider "aws" {
-  region    = "us-east-1"
-  profile   = "terraform-user"
+  region  = "us-east-1"
+  profile = "terraform-user"
 }
 
 # store the terraform state file in s3
 terraform {
   backend "s3" {
-    bucket    = "aosnotes77-terraform-remote-state"
-    key       = "terraform.tfstate.dev"
-    region    = "us-east-1"
-    profile   = "terraform-user"
+    bucket  = "aosnotes77-terraform-remote-state"
+    key     = "fleetcart/terraform.tfstate"
+    region  = "us-east-1"
+    profile = "terraform-user"
   }
 }
